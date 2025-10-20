0d9b8bf96806811f3fb9c3d2d1292ba76e0ca0cd
spykiller <nbje74801@edu.ucl.dk>
2025-10-20 12:28:13 +0200

    Select options

--- Game/ConsoleApp1/Program.cs
+++ Game/ConsoleApp1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TitleScreen screen = new TitleScreen();
+            screen.Titlescreen();
+            screen.ShowTitleScreen();
         }
     }
 }
--- Game/ConsoleApp1/TitleScreen.cs
+++ Game/ConsoleApp1/TitleScreen.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class TitleScreen
+    public class TitleScreen
     {
+        private List<string> menu = new List<string>();
+
+        public List<string> Menu
+        {
+            get { return menu; }
+        }
+        public void Titlescreen()
+        {
+            string grapichs = @"
+   █████████                                        █████                       
+  ███░░░░░███                                      ░░███                        
+ ███     ░░░  ██████  █████████████    ██████       ░███ ██████  █████████████  
+░███         ░░░░░███░░███░░███░░███  ███░░███      ░███░░░░░███░░███░░███░░███ 
+░███    █████ ███████ ░███ ░███ ░███ ░███████       ░███ ███████ ░███ ░███ ░███ 
+░░███  ░░███ ███░░███ ░███ ░███ ░███ ░███░░░  ███   ░██████░░███ ░███ ░███ ░███ 
+ ░░█████████░░█████████████░███ █████░░██████░░████████░░█████████████░███ █████
+  ░░░░░░░░░  ░░░░░░░░░░░░░ ░░░ ░░░░░  ░░░░░░  ░░░░░░░░  ░░░░░░░░░░░░░ ░░░ ░░░░░ ";
+
+            
+            menu.Add(grapichs);
+            menu.Add("\n\nStart");
+            menu.Add("Quit");
+        }
+        public void ShowTitleScreen()
+        {
+            foreach (string item in menu)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void SelectOption()
+        {
+            int[] titleScreenOption = new int[menu.Count];
+
+        }
     }
 }
