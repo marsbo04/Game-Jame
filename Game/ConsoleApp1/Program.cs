e14e48e4e7369969599f62ea051547e7e59165e6
mrka75981 <magnusrk18@gmail.com>
2025-10-20 13:20:48 +0200

    hmm

--- Game/ConsoleApp1/Program.cs
+++ Game/ConsoleApp1/Program.cs
@@ -3,13 +3,8 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            
-            Board board = new (10, 10);
-
-            board.PlaceTerrian(2, 3,"land");
-            board.PlaceTerrian(4, 5,"land");
-            board.Display();
+        {    
+         
 
         }
     }
