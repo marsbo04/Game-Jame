b426dad84b3b6f2bd5a289838b9dab974e838592
Liam Horsmann <liam.horsmann@gmail.com>
2025-10-20 13:26:32 +0200

    rykkede baseboard over i baseboard classen

--- Game/ConsoleApp1/BaseBoard.cs
+++ Game/ConsoleApp1/BaseBoard.cs
@@ -12,7 +12,21 @@ namespace ConsoleApp1
 
           public void SetupBoard()
         {
-            
+            Board board = new(10, 10);
+
+            board.PlaceTerrian(2, 2, "land");
+            board.PlaceTerrian(2, 3, "land");
+            board.PlaceTerrian(3, 2, "land");
+            board.PlaceTerrian(3, 3, "land");
+
+            board.PlaceTerrian(7, 5, "land");
+            board.PlaceTerrian(8, 5, "land");
+            board.PlaceTerrian(8, 6, "land");
+            board.PlaceTerrian(6, 6, "land");
+            board.PlaceTerrian(6, 7, "land");
+            board.PlaceTerrian(7, 6, "land");
+            board.PlaceTerrian(7, 7, "land");
+            board.Display();
         }
 
     }
