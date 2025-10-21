d47eb283831801a44d309c2ad6ab51d3b987b095
spykiller <nbje74801@edu.ucl.dk>
2025-10-21 10:24:40 +0200

    Lavet en hero og givet den navn

--- Game/ConsoleApp1/Program.cs
+++ Game/ConsoleApp1/Program.cs
@@ -4,70 +4,83 @@
     {
         static void Main(string[] args)
         {
-            BaseBoard baseboard = new BaseBoard();
-            Position pos = new Position(0, 5, baseboard.board);
-            bool gameload = true;
-            string[,]? bound = null; // Fix CS0165: Initialize 'bound' to null
-            while (gameload)
+            TitleScreen title = new TitleScreen();
+            int running = title.SelectOption();
+
+            if (running == 0)
             {
-                bool inmainmap = true;
-                bool onland = false;
-                pos.PlaceHeroOnBoard(baseboard.board, "[⛵]");
-                baseboard.DisplayBoard();
+                BaseBoard baseboard = new BaseBoard();
+                Position pos = new Position(0, 5, baseboard.board);
+                Hero hero = new Hero(title.Name);
 
-                while (inmainmap)
+                bool gameload = true;
+                string[,]? bound = null; // Fix CS0165: Initialize 'bound' to null
+                while (gameload)
                 {
-                    pos.MoveByKeyPress();
-                    string[,] grid = new BaseBoard().board.Boardgrid();
-                    if (grid[pos.y, pos.x] == grid[5, 8])
+                    bool inmainmap = true;
+                    bool onland = false;
+                    pos.PlaceHeroOnBoard(baseboard.board, "[⛵]");
+                    baseboard.DisplayBoard();
+
+                    while (inmainmap)
                     {
-                        inmainmap = false;
-                        onland = true;
-                    }
-                }
-                Console.Clear();
-                Petersisland landboard = new Petersisland();
-
-                Position landpos = new Position(0, 5, landboard.board);
-                landpos.PlaceHeroOnBoard(landboard.board, "[⛵]");
-                landboard.board.Display();
-                landpos.PlaceHeroOnBoard(landboard.board, "[@]");
-
-                while (onland)
-                {
-
-                    string[,] grid = landboard.board.Boardgrid();
-                    for (int y = 0; y < landboard.board.Height; y++)
-                    {
-                        for (int x = 0; x < landboard.board.Width; x++)
+                        pos.MoveByKeyPress();
+                        string[,] grid = new BaseBoard().board.Boardgrid();
+                        if (grid[pos.y, pos.x] == grid[5, 8])
                         {
-                            if (y == 0 || y == landboard.board.Height - 1 || x == 0 || x == landboard.board.Width - 1)
-                            {
-                                string[,] landbound = landboard.board.Boardgrid();
-                                bound = landbound;
-                            }
+                            inmainmap = false;
+                            onland = true;
                         }
                     }
-                    if (bound != null && grid[landpos.y, landpos.x] != bound[landpos.y+1, landpos.x+1] && grid[landpos.y, landpos.x] == bound[0,5]) // Fix CS8602: Null check for 'bound'
+                    Console.Clear();
+                    Petersisland landboard = new Petersisland();
+
+                    Position landpos = new Position(0, 5, landboard.board);
+                    landpos.PlaceHeroOnBoard(landboard.board, "[⛵]");
+                    landboard.board.Display();
+                    landpos.PlaceHeroOnBoard(landboard.board, "[@]");
+
+                    while (onland)
                     {
-                        continue;
+
+                        string[,] grid = landboard.board.Boardgrid();
+                        for (int y = 0; y < landboard.board.Height; y++)
+                        {
+                            for (int x = 0; x < landboard.board.Width; x++)
+                            {
+                                if (y == 0 || y == landboard.board.Height - 1 || x == 0 || x == landboard.board.Width - 1)
+                                {
+                                    string[,] landbound = landboard.board.Boardgrid();
+                                    bound = landbound;
+                                }
+                            }
+                        }
+                        if (bound != null && grid[landpos.y, landpos.x] != bound[landpos.y + 1, landpos.x + 1] && grid[landpos.y, landpos.x] == bound[0, 5]) // Fix CS8602: Null check for 'bound'
+                        {
+                            continue;
+                        }
+
+                        landpos.MoveByKeyPress();
+
+                        if (grid[landpos.x, landpos.y] == "[⛵]")
+                        {
+                            inmainmap = true;
+                            onland = false;
+                        }
                     }
+                    Console.Clear();
+                    baseboard = new BaseBoard();
 
-                    landpos.MoveByKeyPress();
-
-                    if (grid[landpos.x, landpos.y] == "[⛵]")
+                    if (Console.ReadKey().Key == ConsoleKey.Escape)
                     {
-                        inmainmap = true;
-                        onland = false;
+                        gameload = false;
                     }
                 }
-                Console.Clear();
-                baseboard = new BaseBoard();
 
-                if (Console.ReadKey().Key == ConsoleKey.Escape)
-                {
-                    gameload = false;
-                }
+            }
+            else
+            {
+                Console.WriteLine("Du har afsluttet spillet!");
             }
         }
     }
